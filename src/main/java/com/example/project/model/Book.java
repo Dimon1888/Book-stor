cb355d9c553a533660e9552032146e675d6acb9f
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2026-07-01 18:51:03 +0200

    completed the task2

--- src/main/java/com/example/project/model/Book.java
+++ src/main/java/com/example/project/model/Book.java
@@ -10,14 +10,14 @@ import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "books")
 @SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id = ?")
-@Where(clause = "is_deleted = false")
+@SQLRestriction("is_deleted = false")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,10 +37,9 @@ public class Book {
 
     private String description;
 
-    @Column(name = "cover_image")
     private String coverImage;
 
-    @Column(name = "is_deleted", columnDefinition = "TINYINT(1)")
+    @Column(nullable = false, columnDefinition = "TINYINT(1)")
     private boolean isDeleted = false;
 
 }
